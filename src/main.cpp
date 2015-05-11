d58d836a182c18de8b0a77ae1949165ce77779d6
Mark <mark@ros.(none)>
2015-05-10 19:08:02 -0500

    cleaned up more stuff. separated out the crazy clustering code from the main logic.

--- src/main.cpp
+++ src/main.cpp
@@ -49,9 +49,6 @@ bool new_cloud_available_flag = false;
 // General point cloud to store the whole image
 PointCloudT::Ptr cloud (new PointCloudT);
 
-//Point Cloud to store out neon cap
-PointCloudT::Ptr neon_cloud (new PointCloudT);
-
 // Message required to publish the cloud - Convert from pcl to msg
 sensor_msgs::PointCloud2 cloud_ros;
 
@@ -97,14 +94,14 @@ double r(const vector<double>& s, geometry_msgs::Twist& a,  const vector<double>
   return reward;
 }
 
-void processDistance(tf::Vector3 vec) {
+void processDistances(vector<tf::Vector3> markers) {
   //wait for the effect of our action
   if((ros::Time::now() - last_command).toSec() < 1./rate) {
     return; 
   }
 
   vector<double> state(1,0.);
-  state[0] = vec[2]; //z is forward distance to object
+  state[0] = markers[0][2]; //right now we're just looking at the first marker's z distance
    
   geometry_msgs::Twist action = controller->computeAction(state);
    
@@ -118,6 +115,81 @@ void processDistance(tf::Vector3 vec) {
 
   last_command = ros::Time::now();
   velocity_pub.publish(action);
+}
+
+
+vector<tf::Vector3> getClusters(PointCloudT::Ptr cloud) {
+  vector<tf::Vector3> centroids;
+  //------------euclidean clustering---------
+  // Create the segmentation object for the planar model and set all the parameters
+  pcl::SACSegmentation<PointT> seg;
+  pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
+  pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
+  pcl::PointCloud<PointT>::Ptr cloud_plane (new pcl::PointCloud<PointT> ());
+  seg.setOptimizeCoefficients (true);
+  seg.setModelType (pcl::SACMODEL_PLANE);
+  seg.setMethodType (pcl::SAC_RANSAC);
+  seg.setMaxIterations (50);
+  seg.setDistanceThreshold (0.1);
+    
+    
+  if(cloud->points.size() < 20) {
+    ROS_INFO("NOT ENOUGH POINTS");
+    return centroids;
+  }
+
+  // Creating the KdTree object for the search method of the extraction
+  pcl::search::KdTree<PointT>::Ptr tree (new pcl::search::KdTree<PointT>);
+  tree->setInputCloud (cloud);
+
+  std::vector<pcl::PointIndices> cluster_indices;
+  pcl::EuclideanClusterExtraction<PointT> ec;
+  ec.setClusterTolerance (0.02); // 2cm
+  ec.setMinClusterSize (20);
+  ec.setMaxClusterSize (2800);
+  ec.setSearchMethod (tree);
+  ec.setInputCloud (cloud);
+  ec.extract (cluster_indices);
+
+  /*  if(cluster_indices.size() < 1) {
+    ROS_INFO("NOT ENOUGH HATS!!!");
+    continue;
+  }*/
+
+  tf::TransformListener listener;
+  for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin (); it != cluster_indices.end(); ++it)
+  {
+    pcl::PointCloud<PointT>::Ptr cloud_cluster (new pcl::PointCloud<PointT>);
+    for (std::vector<int>::const_iterator pit = it->indices.begin (); pit != it->indices.end (); ++pit)
+      cloud_cluster->points.push_back (cloud->points[*pit]); 
+    cloud_cluster->width = cloud_cluster->points.size ();
+    cloud_cluster->height = 1;
+    cloud_cluster->is_dense = true;
+    //std::cout << "PointCloud representing the Cluster: " << cloud_cluster->points.size () << " data points." << std::endl;
+
+    //compute a centroid for each found cluster.
+    Eigen::Vector4f centroid;
+    pcl::compute3DCentroid(*cloud_cluster, centroid);
+    
+    tf::StampedTransform transform;
+    tf::Vector3 transf;
+    tf::Vector3 vec(centroid(0), centroid(1), centroid(2));
+    
+    //transform each found cluster
+
+    try {
+      listener.waitForTransform("base_footprint", "nav_kinect_depth_frame", ros::Time(0), ros::Duration(3.0));
+      listener.lookupTransform("base_footprint", "nav_kinect_depth_frame", ros::Time(0), transform);
+      transf = transform*vec;
+    } catch(tf::TransformException &ex) {
+      ROS_ERROR("%s", ex.what());
+      ros::Duration(1.0).sleep();
+      continue;
+    }
+    centroids.push_back(transf);
+  }
+
+  return centroids;
 }
 
 int main (int argc, char** argv)
@@ -142,7 +214,6 @@ int main (int argc, char** argv)
   //refresh rate
   double ros_rate = 5.0;
   ros::Rate r(ros_rate);
-  tf::TransformListener listener;
   
   while (ros::ok())
   {
@@ -150,11 +221,9 @@ int main (int argc, char** argv)
     r.sleep();
 
     if (new_cloud_available_flag){
+      //cloud contains a new image to process.
+      new_cloud_available_flag = false; //reset this
 
-      new_cloud_available_flag = false;
-
-      // Voxel Grid reduces the computation time. Its a good idea to do it if you will be doing
-      //sequential processing or frame-by-frame
       // Create the filtering object: downsample the dataset using a leaf size of 1cm
       pcl::VoxelGrid<PointT> vg;
       pcl::PointCloud<PointT>::Ptr cloud_filtered (new pcl::PointCloud<PointT>);
@@ -163,81 +232,16 @@ int main (int argc, char** argv)
       vg.filter (*cloud_filtered);
       
       //Send the filtered point cloud to be processed in order to get the neon blob
-      neon_cloud = computeNeonVoxels(cloud_filtered);
+      PointCloudT::Ptr neon_cloud = computeNeonVoxels(cloud_filtered);
       
-      
-      //------------euclidean clustering---------
-      // Create the segmentation object for the planar model and set all the parameters
-      pcl::SACSegmentation<PointT> seg;
-      pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
-      pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
-      pcl::PointCloud<PointT>::Ptr cloud_plane (new pcl::PointCloud<PointT> ());
-      seg.setOptimizeCoefficients (true);
-      seg.setModelType (pcl::SACMODEL_PLANE);
-      seg.setMethodType (pcl::SAC_RANSAC);
-      seg.setMaxIterations (50);
-      seg.setDistanceThreshold (0.1);
-        
-        
-      if(neon_cloud->points.size() < 20) {
-        ROS_INFO("NOT ENOUGH POINTS");
+      vector<tf::Vector3> clusterCentroids = getClusters(neon_cloud);
+
+      if(clusterCentroids.size() < 1) {
+        ROS_INFO("NOT ENOUGH MARKERS");
         continue;
       }
 
-      // Creating the KdTree object for the search method of the extraction
-      pcl::search::KdTree<PointT>::Ptr tree (new pcl::search::KdTree<PointT>);
-      tree->setInputCloud (neon_cloud);
-
-      std::vector<pcl::PointIndices> cluster_indices;
-      pcl::EuclideanClusterExtraction<PointT> ec;
-      ec.setClusterTolerance (0.02); // 2cm
-      ec.setMinClusterSize (20);
-      ec.setMaxClusterSize (2800);
-      ec.setSearchMethod (tree);
-      ec.setInputCloud (neon_cloud);
-      ec.extract (cluster_indices);
-
-      int j = 0;
-      int size = 0;
-      if(cluster_indices.size() < 1) {
-        ROS_INFO("NOT ENOUGH HATS!!!");
-        continue;
-      }
-
-      tf::Vector3 firstHat;
-      bool foundOneHat = false;
-      for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin (); it != cluster_indices.end(); ++it)
-      {
-        pcl::PointCloud<PointT>::Ptr cloud_cluster (new pcl::PointCloud<PointT>);
-        for (std::vector<int>::const_iterator pit = it->indices.begin (); pit != it->indices.end (); ++pit)
-          cloud_cluster->points.push_back (neon_cloud->points[*pit]); 
-        cloud_cluster->width = cloud_cluster->points.size ();
-        cloud_cluster->height = 1;
-        cloud_cluster->is_dense = true;
-        //std::cout << "PointCloud representing the Cluster: " << cloud_cluster->points.size () << " data points." << std::endl;
-        j++;
-        //compute a centroid for each found cluster.
-        Eigen::Vector4f centroid;
-        pcl::compute3DCentroid(*cloud_cluster, centroid);
-        
-        tf::StampedTransform transform;
-        tf::Vector3 transf;
-        tf::Vector3 vec(centroid(0), centroid(1), centroid(2));
-      
-        //transform each found cluster
-        try {
-          listener.waitForTransform("base_footprint", "nav_kinect_depth_frame", ros::Time(0), ros::Duration(3.0));
-          listener.lookupTransform("base_footprint", "nav_kinect_depth_frame", ros::Time(0), transform);
-          transf = transform*vec;
-        } catch(tf::TransformException &ex) {
-          ROS_ERROR("%s", ex.what());
-          ros::Duration(1.0).sleep();
-          continue;
-        }
-      
-        processDistance(transf);
-        
-      }
+      processDistances(clusterCentroids); //right now there's only one marker but there will be more later
       
       pcl::toROSMsg(*neon_cloud,cloud_ros);
       
